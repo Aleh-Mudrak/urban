c2d007564fb59ec47f567e567f6a7d5908cc979c
Aleh Mudrak <o.mudrak@outlook.com>
2022-07-22 15:44:57 +0100

    Reconfigure tf-code

--- tf-code/test/datasources.tf
+++ tf-code/test/datasources.tf
@@ -1,3 +1,4 @@
+# Get data from remote tfstate file
 
 data "terraform_remote_state" "infrustructure" {
   backend = "gcs"
@@ -8,5 +9,6 @@ data "terraform_remote_state" "infrustructure" {
 }
 
 locals {
+  # test
   service_account_id = data.terraform_remote_state.infrustructure.outputs.service_account_id
 }
