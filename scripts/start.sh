8629b0326686e69d79ebf2de7f4bc884be9d6917
Aleh Mudrak <o.mudrak@outlook.com>
2022-08-11 18:18:48 +0300

    Updated folders structure.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,39 +1,47 @@
-# Initial parameters to Deploy Infrastructure by CLI and Terraform code
-
 #!/bin/bash
+### Initial parameters to Deploy Infrastructure by CLI and Terraform code
+# This script have to run from folder "urban/scripts" only one time for deploy infrustructure in GCP
+
 set -u
 
-ScriptStarted="$(date +%s)"
 ### Variables
-export project_id="taskurban"       # project ID
+ScriptStarted="$(date +%s)"
+startFolder=$PWD
+
+export project_id="taskurban"     # project ID
 export bucket="tfstate_files"     # backet
 infr_prefix="infrustructure"      # infrustructure prefix in bucket
-deploy_prefix="my_deploy"            # deploy prefix in bucket
+deploy_prefix="deploy"            # deploy prefix in bucket
 export region="us-central1"       # backet region
 tfvars_infr="../infr.tfvars"      # path to infrustructure tfvars-file
 tfvars_deploy="../deploy.tfvars"  # path to deploy tfvars-file
 
-# Initialization
-startFolder=$PWD
-cd tf-code/
-./init.sh
 
-# Create infrustructure
-cd infrustructure
+### Initialization
+$startFolder/init.sh
+exitCode="$?"              # Check script status
+[ $exitCode != 0 ] && echo "Check initialization step" && exit 1
+
+
+### Create infrustructure
+cd ../tf-code/infrustructure
 # Change bucket name and prefix in backend
 sed -i "s|taskurban|$project_id|g" $tfvars_infr  # update Project ID in tfvars
 sed -i "s|tfstate_files|$bucket|g" main.tf       # update bucket name
 sed -i "s|infrustructure|$infr_prefix|g" main.tf # update bucket prefix
 # Start script
 export tfvars=$tfvars_infr
-./../infrustructure.sh
+$startFolder/infrustructure.sh
+exitCode="$?"              # Check script status
+[ $exitCode != 0 ] && echo "Check initialization step" && exit 1
 # Get Variables for GitHub Actions
 service_account=$(terraform output -raw service_account_sa_key)  # GKE_SA_KEY
 cluster_location=$(terraform output -raw cluster_location)  # GKE_ZONE
 cluster_name=$(terraform output -raw cluster_name)  # GKE_CLUSTER
 # $project_id = GKE_PROJECT
 
-# Deploy in Cluster
+
+### Deploy in Cluster
 cd ../deploy
 # Change bucket name and prefix in backend
 sed -i "s|tfstate_files|$bucket|g" $tfvars_deploy   # update bucket in tfvars
@@ -41,9 +49,12 @@ sed -i "s|tfstate_files|$bucket|g" main.tf          # update bucket name
 sed -i "s|infrustructure|$deploy_prefix|g" main.tf  # update bucket prefix
 # Start script
 export tfvars=$tfvars_deploy
-./../deploy.sh
+$startFolder/deploy.sh
+exitCode="$?"              # Check script status
+[ $exitCode != 0 ] && echo "Check initialization step" && exit 1
 
-# Finish
+
+### Finish
 cd $startFolder
 echo -e "\n=== Copy this Variables and past in GitHub Repository Secrets"
 echo "GKE_PROJECT = $project_id"
